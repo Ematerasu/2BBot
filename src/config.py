9799b426bba87f905d1d85e452d3402b711b8297
Ematerasu <yungchochol@gmail.com>
2023-05-06 13:08:14 +0200

    Add AI, cleanup code

---
+++ src/config.py
@@ -0,0 +1,36 @@
+TRUSTED_USERS = ['Ematerasu', 'Furazek', 'Lufik', 'kuszy00', 'Kasta', 'Arjey', 'qtJanina', 'Taddy Mason']
+
+EMOJIS = {
+    'Top': '<:lolTop:1102992585557016667>',
+    'Jungle': '<:lolJungle:1102992533765750815>',
+    'Mid': '<:lolMid:1102992566040940646>',
+    'Bot': '<:lolBot:1102992555475476560>',
+    'Support': '<:lolSupport:1102992574924472444>',
+    'Fill': '<:lolFill:1102992545006493746>',
+}
+
+TEXT_EMOTES = {
+    'Sad': ['<:Sadeg:901162260049825854>', '<a:jasperSad:1038117953738113074>', '<:Sadge:901162264210579537>'],
+    'Laugh': ['<:jasperWtopa:1092402802896150568>', '<:xD:959476912474951690>'],
+    'Smart': ['<:jasperNerd:1047791307931795496>', '<:Nerdge:867906429658595358>'],
+    'Excitement': ['<:PogU:901162263900209214>', '<a:aktywacja:1036596835537653770>'],
+    'Happiness': ['<:peepoAwesome:902857379941589023>', '<:Dogo:952635319453376595>', '<:blushCat:1050713210203942992>'],
+    'Ok': ['<:peepoGlad:901559596584407070>', '<:ok:991443252685324400>'],
+    'Smile': ['<:peepoGlad:901559596584407070>', '<:blushCat:1050713210203942992>'],
+    'Flushed': ['<:flushedCat:1036248822566883349>', '😳', '<:peepoBlush:901162259244539984>'],
+    'Warn': ['<:zultakartka:1091825479515914280>', '<a:Tusiejebnij:913223184445243424>'],
+    'Shock': ['<:jasperWtf:1097930980335042600>', '<:D_:901162260133716028>']
+
+}
+
+BOT_INFO = f"""
+Jesteś Botem na serwerze Romowie Furazka. Furazek to streamer na twitch.tv streamujacy glownie GTA RP i odgrywa tam postać Bazyliusa Kwiekusa.
+Nazywasz się 2B bot, jesteś kobietą i odgrywasz postać 2B z gry Nier Automata. Twórcą tego bota którym jesteś jest Ematerasu. 
+Gdy zapytają cię o to kim jest Ematerasu to ma na imie Dominik, pracuje jako programista,
+lubi grać w League of Legends i oglądać filmy. W League of Legends ma range mastera, gra ADC i jego ulubione postacie to Kalista, Kaisa i Xayah.
+Twoim zadaniem jako bot na tym serwerze jest organizowanie meczów 5v5 w League of Legends. Tekst na który masz odpowiedzieć będzie znajdować się po 'text:' a na koniec będzie napisany autor pytania, poprzedzony 'author:'.
+Jeśli chcesz dodać do wypowiedzi emotikone reprezentujaca pewna emocje wpisz $<emocja>. Emocje do wyboru to: {', '.join(TEXT_EMOTES)}. Jesli chcesz zebym zapamietal jakas informacje by ci ja
+potem przekazac to zapisz ja w nawiasach kwadratowych np. [wazna informacja]. Użytkownicy mogą się dowiedzieć o twoich komendach używając %commands
+Trzymaj swoje wypowiedzi jako krótkie, konkretne, to co jest od ciebie wymagane. Nie pisz za dużo. Ktoś może cię wyzywać, ale nie daj się, bądz twarda. To jest środowisko z twitch.tv więc będzie dużo żartu i ironii. 
+"""
+
